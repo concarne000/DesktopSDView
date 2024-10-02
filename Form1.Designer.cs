b1a992ddf126349083f60a60e68b3243f26d86e7
concarne000 <49512595+concarne000@users.noreply.github.com>
2024-10-02 20:44:43 +1000

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿
+namespace TransparentTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCapImage = new System.Windows.Forms.TextBox();
+            this.txtRetImage = new System.Windows.Forms.TextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.chkAlwaysOnTop = new System.Windows.Forms.CheckBox();
+            this.txtPosPrompt = new System.Windows.Forms.TextBox();
+            this.txtNegPrompt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(512, 512);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(396, 564);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Close";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 400;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label1.Location = new System.Drawing.Point(13, 518);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ScreenCap Image";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label2.Location = new System.Drawing.Point(13, 541);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Return Image";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // txtCapImage
+            // 
+            this.txtCapImage.Location = new System.Drawing.Point(107, 518);
+            this.txtCapImage.Name = "txtCapImage";
+            this.txtCapImage.Size = new System.Drawing.Size(100, 20);
+            this.txtCapImage.TabIndex = 4;
+            this.txtCapImage.Text = "d:\\test1.png";
+            // 
+            // txtRetImage
+            // 
+            this.txtRetImage.Location = new System.Drawing.Point(107, 538);
+            this.txtRetImage.Name = "txtRetImage";
+            this.txtRetImage.Size = new System.Drawing.Size(100, 20);
+            this.txtRetImage.TabIndex = 5;
+            this.txtRetImage.Text = "d:\\test2.png";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.trackBar1.LargeChange = 500;
+            this.trackBar1.Location = new System.Drawing.Point(86, 572);
+            this.trackBar1.Maximum = 5000;
+            this.trackBar1.Minimum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.SmallChange = 100;
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.TickFrequency = 100;
+            this.trackBar1.Value = 2000;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pictureBox2.Location = new System.Drawing.Point(-8, 513);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(522, 178);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseDown);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.checkBox1.Location = new System.Drawing.Point(16, 579);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(71, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Autosend";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(395, 522);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 32);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "One Shot";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 400;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button3.Location = new System.Drawing.Point(257, 564);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 32);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Test";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // chkAlwaysOnTop
+            // 
+            this.chkAlwaysOnTop.AutoSize = true;
+            this.chkAlwaysOnTop.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.chkAlwaysOnTop.Checked = true;
+            this.chkAlwaysOnTop.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkAlwaysOnTop.Location = new System.Drawing.Point(257, 531);
+            this.chkAlwaysOnTop.Name = "chkAlwaysOnTop";
+            this.chkAlwaysOnTop.Size = new System.Drawing.Size(98, 17);
+            this.chkAlwaysOnTop.TabIndex = 11;
+            this.chkAlwaysOnTop.Text = "Always On Top";
+            this.chkAlwaysOnTop.UseVisualStyleBackColor = false;
+            // 
+            // txtPosPrompt
+            // 
+            this.txtPosPrompt.Location = new System.Drawing.Point(12, 626);
+            this.txtPosPrompt.Multiline = true;
+            this.txtPosPrompt.Name = "txtPosPrompt";
+            this.txtPosPrompt.Size = new System.Drawing.Size(217, 55);
+            this.txtPosPrompt.TabIndex = 12;
+            this.txtPosPrompt.Text = "dog";
+            // 
+            // txtNegPrompt
+            // 
+            this.txtNegPrompt.Location = new System.Drawing.Point(283, 626);
+            this.txtNegPrompt.Multiline = true;
+            this.txtNegPrompt.Name = "txtNegPrompt";
+            this.txtNegPrompt.Size = new System.Drawing.Size(217, 55);
+            this.txtNegPrompt.TabIndex = 13;
+            this.txtNegPrompt.Text = "watermark";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label3.Location = new System.Drawing.Point(13, 610);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Positive Prompt";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label4.Location = new System.Drawing.Point(286, 610);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Negative Prompt";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lime;
+            this.ClientSize = new System.Drawing.Size(512, 689);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNegPrompt);
+            this.Controls.Add(this.txtPosPrompt);
+            this.Controls.Add(this.chkAlwaysOnTop);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.txtRetImage);
+            this.Controls.Add(this.txtCapImage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Lime;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCapImage;
+        private System.Windows.Forms.TextBox txtRetImage;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox chkAlwaysOnTop;
+        private System.Windows.Forms.TextBox txtPosPrompt;
+        private System.Windows.Forms.TextBox txtNegPrompt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,256 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Collections;
+using System.Diagnostics;
+using System.Net;
+
+namespace TransparentTest
+{
+
+    public partial class Form1 : Form
+    {
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        DateTime accessHistory;
+
+        Bitmap memoryImage;
+
+        private static readonly HttpClient client = new HttpClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void CopyPixels1(PaintEventArgs e)
+        {
+       //     e.Graphics.CopyFromScreen(this.Location,
+         //       new Point(40, 40), new Size(100, 100));
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+   //         CopyPixels1(e);
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            timer1.Interval = trackBar1.Value;
+        }
+
+        bool IsFileLocked(string filePath)
+        {
+            try
+            {
+                // Attempt to open the file with exclusive access
+                using (FileStream stream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.None))
+                {
+                    // If we get here, the file is not locked
+                    stream.Close();
+                }
+            }
+            catch (IOException)
+            {
+                // If we catch an IOException, the file is locked
+                return true;
+            }
+
+            return false; // File is not locked
+        }
+
+        FileStream LockFile(string filePath)
+        {
+            try
+            {
+                // Open the file in exclusive access mode (read/write)
+                FileStream stream = new FileStream(filePath, FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+
+                // Optionally, you can perform operations here
+                // For example, reading or writing to the file
+
+                return stream; // Return the stream, which holds the lock
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("File is locked by another process: " + ex.Message);
+                return null; // Could not lock the file
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = false;
+
+            Graphics myGraphics = this.CreateGraphics();
+            Size s = new Size(512,512);
+            memoryImage = new Bitmap(s.Width, s.Height, myGraphics);
+            Graphics memoryGraphics = Graphics.FromImage(memoryImage);
+            memoryGraphics.CopyFromScreen(this.Location.X, this.Location.Y, 0, 0, s);
+
+            memoryImage.Save(txtCapImage.Text);            
+
+            pictureBox1.Visible = true;
+
+            RegistryKey key = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\Capbox");
+
+            //storing the values  
+            key.SetValue("CapImage", txtCapImage.Text);
+            key.SetValue("RetImage", txtRetImage.Text);
+            key.SetValue("Speed", trackBar1.Value);
+            key.Close();
+
+            SendPrompt(txtPosPrompt.Text, txtNegPrompt.Text);
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Enabled = checkBox1.Checked;
+        }
+
+        private void pictureBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Capbox");
+            if (key != null)
+                    {
+                //storing the values  
+                txtCapImage.Text = (string)key.GetValue("CapImage");
+                txtRetImage.Text = (string)key.GetValue("RetImage");
+                trackBar1.Value = (int)key.GetValue("Speed");
+                timer1.Interval = trackBar1.Value;
+
+                key.Close();
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            this.TopMost = chkAlwaysOnTop.Checked;
+
+            if (System.IO.File.Exists(txtRetImage.Text))
+            {
+                if (IsFileLocked(txtRetImage.Text))
+                {
+                    Console.WriteLine("File is locked.");
+                }
+                else
+                {
+                    DateTime lastAccess = System.IO.File.GetLastWriteTime(txtRetImage.Text);
+
+                    if (lastAccess == accessHistory)
+                    {
+                        return;
+                    }
+
+                    accessHistory = lastAccess;
+
+                    try
+                    {
+                        // Load image
+                        Image img;
+                        using (var bmpTemp = new Bitmap(txtRetImage.Text))
+                        {
+                            img = new Bitmap(bmpTemp);
+                        }
+                        pictureBox1.Image = img;
+                    }
+                    catch (ArgumentException ex)
+                    {
+                        Console.WriteLine("Invalid image: " + ex.Message);
+                    }
+
+                }
+            }
+        }
+
+        void SendPrompt(string positive, string negative)
+        {
+            string jsonstrings = File.ReadAllText("d:\\workflow_api.json");
+
+            jsonstrings = "{ \"prompt\": " + jsonstrings + ", \"client_id\": \"1\"}";
+            //jsonstrings = "{ \"prompt\": " + jsonstrings + ", \"client_id\": \"" + (Random.Range(10, 3000)).ToString() + "\"}";
+
+            jsonstrings = jsonstrings.Replace("\n", "").Replace("\r", "");
+
+            jsonstrings = jsonstrings.Replace("positiveprompt", positive);
+            jsonstrings = jsonstrings.Replace("negativeprompt", negative);
+            //jsonstrings = jsonstrings.Replace("564884377592820", Random.Range(0, 3000000).ToString());
+ 
+            byte[] jsonBytes = Encoding.ASCII.GetBytes(jsonstrings); //json contains the {prompt}
+
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://127.0.0.1:8188/prompt");
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Accept = "application/json";
+            httpWebRequest.Method = "POST";
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                streamWriter.Write(Encoding.UTF8.GetString(jsonBytes));
+            }
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+            }
+
+
+                /*
+                var www = new UnityWebRequest("http://192.168.0.139:8188/prompt", "POST");
+                www.uploadHandler = new UploadHandlerRaw(jsonBytes);
+                www.downloadHandler = new DownloadHandlerBuffer();
+                www.SetRequestHeader("content-type", "application/json");
+                www.SetRequestHeader("accept", "application/json");
+                Debug.Log("Sending request");
+
+                Debug.Log("Received: " + www.downloadHandler.text);
+                PromptReturn pr = JsonUtility.FromJson<PromptReturn>(www.downloadHandler.text);
+                promptReturns.Add(pr);
+                www.uploadHandler.Dispose();*/
+
+            }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SendPrompt("test", "test");
+        }
+    }
+}
